d2984ac5be4b56a91901955474dbcaa9da458034
David Artunduaga <1040871155@u.icesi.edu.co>
2024-11-16 17:49:52 -0500

    chore: protect productRoutes

--- src/api/routes/productRoutes.ts
+++ src/api/routes/productRoutes.ts
@@ -1,13 +1,14 @@
 // src/api/routes/productRoutes.ts
 import express from 'express';
 import { getAllProducts, createProduct, getProductById, updateProduct, deleteProduct } from '../controllers/productController';
+import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);          // Obtener todos los productos
-router.post('/', createProduct);          // Crear un nuevo producto
+router.post('/', authenticateToken, createProduct); // Crear un nuevo producto
 router.get('/:id', getProductById);       // Obtener un producto específico por ID
-router.put('/:id', updateProduct);        // Actualizar un producto específico por ID
-router.delete('/:id', deleteProduct);     // Eliminar un producto específico por ID
+router.put('/:id', authenticateToken, updateProduct);        // Actualizar un producto específico por ID
+router.delete('/:id', authenticateToken, deleteProduct);     // Eliminar un producto específico por ID
 
 export default router;
