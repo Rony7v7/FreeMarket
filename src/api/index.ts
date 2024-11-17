3e9145df88ae068be6af68f90255d4cf9e4788b6
Rony Ordoñez <ronyfarido@gmail.com>
2024-11-17 16:10:48 -0500

    feat: configure server output and add CORS support in API

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,16 @@ import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
 import orderRoutes from './routes/orderRoutes';
 
+import cors from 'cors';
+
 const api = express();
 api.use(express.json());
+api.use(cors(
+    {
+        origin: 'http://localhost:4321',
+        credentials: true
+    }
+));
 
 // api.use('/users', userRoutes);
 api.use('/api/users', userRoutes);
